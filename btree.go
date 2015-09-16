9ff561d66b79712e2e0cdd2e39fe29f349ede7c0
He Fei <droxer.he@gmail.com>
2015-09-17 00:42:08 +0800

    insertion is done.

--- btree.go
+++ btree.go
@@ -24,26 +24,89 @@ func (s items) find(item Item) (index int, found bool) {
 	return i, false
 }
 
-// Node represents single node in BTree.
-type Node struct {
-	children []*Node
+func (s *items) insertAt(i int, item Item) {
+	*s = append(*s, nil)
+
+	if i < len(*s) {
+		copy((*s)[i+1:], (*s)[i:])
+	}
+	(*s)[i] = item
+}
+
+type children []*node
+
+func (s *children) insertAt(i int, n *node) {
+	*s = append(*s, nil)
+
+	if i < len(*s) {
+		copy((*s)[i+1:], (*s)[i:])
+	}
+	(*s)[i] = n
+}
+
+// node represents single node in BTree.
+type node struct {
+	children children
 	items    items
 	t        *BTree
 }
 
-func (n *Node) insert(item Item) {
+func (n *node) split(i int) (Item, *node) {
+	item := n.items[i]
+	next := n.t.newNode()
+	next.items = append(next.items, n.items[i+1:]...)
+	n.items = n.items[:i]
+
+	if len(n.children) > 0 {
+		next.children = append(next.children, n.children[i+1:]...)
+		n.children = n.children[:i+1]
+	}
+
+	return item, next
+}
+
+func (n *node) maybeSplit(i int) bool {
+	if len(n.children[i].items) < n.t.max() {
+		return false
+	}
+
+	first := n.children[i]
+	item, second := first.split(n.t.max() / 2)
+	n.items.insertAt(i, item)
+	n.children.insertAt(i+1, second)
+	return true
+}
+
+func (n *node) insert(item Item) {
 	i, found := n.items.find(item)
 	if found {
 		n.items[i] = item
 		return
 	}
 
+	if len(n.children) == 0 {
+		n.items.insertAt(i, item)
+		return
+	}
+
+	if n.maybeSplit(i) {
+		inItem := n.items[i]
+		switch {
+		case item.Less(inItem):
+		case inItem.Less(item):
+			i++
+		default:
+			n.items[i] = item
+			return
+		}
+	}
+
+	n.children[i].insert(item)
 }
 
 type BTree struct {
 	degree int
-	length int
-	root   *Node
+	root   *node
 }
 
 func New(degree int) *BTree {
@@ -53,6 +116,12 @@ func New(degree int) *BTree {
 
 	return &BTree{
 		degree: degree,
+	}
+}
+
+func (b *BTree) newNode() *node {
+	return &node{
+		t: b,
 	}
 }
 
@@ -70,16 +139,17 @@ func (b *BTree) Insert(item Item) bool {
 	}
 
 	if b.root == nil {
-		b.root = &Node{t: b}
+		b.root = b.newNode()
 		b.root.items = append(b.root.items, item)
-		b.length++
 		return true
+	} else if len(b.root.items) >= b.max() {
+		newItem, second := b.root.split(b.max() / 2)
+		oldRoot := b.root
+		b.root = b.newNode()
+		b.root.items = append(b.root.items, newItem)
+		b.root.children = append(b.root.children, oldRoot, second)
 	}
 
-	if len(b.root.items) < b.max() {
-		b.root.insert(item)
-		return true
-	}
-
+	b.root.insert(item)
 	return true
 }
--- btree_test.go
+++ btree_test.go
@@ -1,7 +1,8 @@
 package btree
 
 import (
-	"testing"
+	"fmt"
+	"strings"
 )
 
 type item int
@@ -10,14 +11,37 @@ func (i item) Less(other Item) bool {
 	return i < other.(item)
 }
 
-func TestAddFirstNode(t *testing.T) {
-	i := item(2)
+func print(b *BTree) {
+	printNode(b.root, 0)
+}
 
-	btree := New(2)
-	btree.Insert(i)
-
-	found := btree.root.items[0].(item)
-	if found != i {
-		t.Errorf("expected %s, actual is %s", i, found)
+func printNode(n *node, level int) {
+	var items []string
+	for _, v := range n.items {
+		items = append(items, fmt.Sprintf("%v", v))
 	}
+
+	fmt.Printf("level %d: %s\n", level, strings.Join(items, "--"))
+
+	level++
+	for _, v := range n.children {
+		printNode(v, level)
+	}
+}
+
+func ExamplePrint() {
+	btree := New(2)
+	btree.Insert(item(1))
+	btree.Insert(item(2))
+	btree.Insert(item(3))
+	btree.Insert(item(4))
+	btree.Insert(item(5))
+	btree.Insert(item(8))
+	btree.Insert(item(9))
+
+	print(btree)
+	// Output: level 0: 2--4
+	// level 1: 1
+	// level 1: 3
+	// level 1: 5--8--9
 }
